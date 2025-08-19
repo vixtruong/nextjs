7f26259c35fc33813949522a4f29c2cd450fbc3d
vixtruong <vipro20161@gmail.com>
2025-08-19 14:52:46 +0700

    feat: ShadcnUI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Header from "@/app/Header";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Header></Header>
+        <Button>Test ShadcnUI Button</Button>
         <Image
           className="dark:invert"
           src="/next.svg"
